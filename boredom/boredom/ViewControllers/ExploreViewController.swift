de4db34b9e47bca8a830a3fa6312a35936d185fe
Melissa Nguyen <nguyen.melissa23@gmail.com>
2018-04-28 15:56:33 -0700

    random

--- boredom/boredom/ViewControllers/ExploreViewController.swift
+++ boredom/boredom/ViewControllers/ExploreViewController.swift
@@ -24,6 +24,8 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
     var top10List: [List]! = []
     var top10Act: [Activity]! = []
     var bgURL: [String] = ["https://i.imgur.com/2GOE7w9.png", "https://imgur.com/spLeglN.png", "https://imgur.com/SVdeXmg.png", "https://imgur.com/es6rQag.png", "https://imgur.com/VrD2OI3.png", "https://imgur.com/HkECUoG.png", "https://imgur.com/J8lQzBz.png", "https://imgur.com/jpdbJvU.png", "https://imgur.com/3Qm9GDx.png"]
+    var index1 = [Int]()
+    var index2 = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,28 +54,12 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
         //let width = userListsCollectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
         layoutActivities.itemSize = CGSize(width: 120, height: 120)
         
-        
-        fetchMovies()
-        //getTopLists()
-        /*Business.searchWithTerm(term: "Thai", completion: { (activitiesYelp: [Business]?, error: Error?) -> Void in
-            
-            self.activitiesYelp = activitiesYelp
-            self.activitiesCollectionView.reloadData()
-            if case let self.activitiesYelp = activitiesYelp {
-                for self.activitiesYelp in activitiesYelp {
-                    print("BUSINESSES")
-                }
-            }
-            
-        }
-        )*/
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidLoad()
         
+//        randomizeBG()
         userListsCollectionView.dataSource = self
         activitiesCollectionView.dataSource = self
         
@@ -92,15 +78,12 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
         let interItemSpacingTotalActivities = layoutActivities.minimumInteritemSpacing * (cellsPerLineActivities - 1)
         //let width = userListsCollectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
         layoutActivities.itemSize = CGSize(width: 120, height: 120)
-        
-        
-        fetchMovies()
+
         getTopLists()
         getTopActivities()
     }
     
     func getTopLists(){
-        //print(".............INSIDE TOP LISTS.........")
         List.fetchLists { (lists: [List]?, error: Error?) in
             if error == nil {
                 self.exploreLists = lists
@@ -130,27 +113,18 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
                     //self.exploreActivities = activities
                     let activities = activities
                     var i = 0
-                    while i < (activities?.count)! {
+                    while i < 10{
                         print("activityLikeCount", activities![i].activityLikeCount)
                         let act = activities![i]
                         self.top10Act.append(act)
                         self.activitiesCollectionView.reloadData()
                         i = i + 1
-                        
                     }
-
                 }
-                
             }
-            
         }
-
     }
-    
-    
-    
 
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -158,7 +132,6 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if(collectionView == activitiesCollectionView){
-        
             return top10Act.count
         }
         else{
@@ -182,24 +155,13 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
         
         else{
             let activitiesCell = activitiesCollectionView.dequeueReusableCell(withReuseIdentifier: "ActivitiesCell", for: indexPath) as! ActivitiesCell
-//            print(indexPath.item)
-//            print("count: ", top10Act.count)
             let act = top10Act[indexPath.item]
-//            print("act = top10Act[indexPath.item] ",act)
             activitiesCell.activityName.text = act.actName ?? "Label"
             let randomindex = Int(arc4random_uniform(UInt32(bgURL.count)) )
             let background = bgURL[randomindex]
             let backgroundURL = URL(string: background)
             activitiesCell.activitiesImageView.af_setImage(withURL: backgroundURL!)
             
-//            let movie = movies[indexPath.item]
-//            if let posterPathString = movie["poster_path"] as? String {
-//                let baseURLString = "https://image.tmdb.org/t/p/w500"
-//                let posterURL = URL(string: baseURLString + posterPathString)!
-//                activitiesCell.activitiesImageView.af_setImage(withURL: posterURL)
-           // activitiesCell.activitiesImageView.af_setImage(withURL: URL(string: self.exploreActivity.actImageUrl)!)
-//            }
-            //activitiesCell.business = activitiesYelp[indexPath.item]
             return activitiesCell
         }
         
@@ -215,16 +177,12 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
             let activityDetailViewController = segue.destination as! ActivitiesDetailViewController
             print(activity)
             activityDetailViewController.activity = activity
-            //print(activityDetailViewController.activity)
-            
-            //activityDetailViewController.activityName.text = activity.actName ?? "Label"
         }
         else if let indexPath = userListsCollectionView.indexPath(for: listCell){
             let list = top10List[indexPath.item]
             let listDetailViewController = segue.destination as! ListsDetailViewController
             listDetailViewController.list = list
             listDetailViewController.authorOfList = list.author
-            
         }
     }
     
@@ -234,72 +192,27 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource{
         appDelegate.logout()
     }
     
-    
-    func fetchMovies()
-    {
-        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        
-        //getting data
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-        //task to get the data
-        let task = session.dataTask(with: request) { (data, response, error) in
-            //This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-                //based on error, display corresponding message
-                
-                //create a alert view controller
-                let alertController = UIAlertController(title: "Cannot load movies", message: "Please check your wifi connection", preferredStyle: .alert)
-                
-                //create an OK action
-                let OKAction = UIAlertAction(title: "OK", style: .default) {(action) in
-                    //handle response here
-                }
-                
-                // add the OK action to the alert controller
-                alertController.addAction(OKAction)
-                
-                
-                DispatchQueue.main.async {
-                    self.present(alertController, animated: true, completion: nil)
-                }
-            }
-            else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
-                //                print(dataDictionary)
-                let movies = dataDictionary["results"] as! [[String:Any]]
-                self.movies = movies
-                self.userListsCollectionView.reloadData()
-                self.activitiesCollectionView.reloadData()
-                //.refreshControl.endRefreshing()
-                
-            }
+    func randomizeBG (){
+        var i = 0
+        var j = 0
+        while i < top10Act.count {
+            let randomindex = Int(arc4random_uniform(UInt32(bgURL.count)))
+            index1.append(randomindex)
+            i += 1
         }
-        //ALWAYS NEED TO CALL THIS FUNCTION! this will actually start the task
-        task.resume()
+        while j < top10Act.count {
+            let randomindex = Int(arc4random_uniform(UInt32(bgURL.count)))
+            index2.append(randomindex)
+            j += 1
+        }
+        print ("index 2", index2)
+        print("index1", index1)
     }
     
-    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "listDetailSegue" {
-            let cell = sender as! UICollectionViewCell
-            if let indexPath = userListsCollectionView.indexPath(for: cell) {
-                let list = top10List[indexPath.item]
-                let detailsViewController = segue.destination as! ListsDetailViewController
-                detailsViewController.list = list
-            }
-        }
-    }*/
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
