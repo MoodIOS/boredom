c6172a1fde02e265f294edd82f93ceda83f4fa90
justin.lee@corebaby.org <TURNIPZ@Justins-MacBook-Pro-3.local>
2018-04-27 17:40:38 -0700

    figured out option view controlller, havent done categories tho

--- boredom/boredom/ViewControllers/OptionsViewController.swift
+++ boredom/boredom/ViewControllers/OptionsViewController.swift
@@ -23,6 +23,12 @@ class OptionsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let saved = UserDefaults.standard
+        let which = saved.integer(forKey: "whichOne")
+        let whichTwo = saved.integer(forKey: "whichTwo")
+
+        costControl.selectedSegmentIndex = which
+        distanceControl.selectedSegmentIndex = whichTwo
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,16 +38,49 @@ class OptionsViewController: UIViewController {
     
     
     @IBAction func saveAction(_ sender: Any) {
+        //Cost Segmented Control
+        let choseMon = [1, 2, 3, 4]
+        let result = choseMon[costControl.selectedSegmentIndex]
+        let savedBool = UserDefaults.standard
+        if(result == 1){
+            savedBool.set(0, forKey: "whichOne")
+            print("yah")
+        }
+        else if(result == 2){
+            savedBool.set(1, forKey: "whichOne")
+            print("yah1")
+        }
+        else if(result == 3){
+            savedBool.set(2, forKey: "whichOne")
+            print("yah1")
+        }
+        else{
+            savedBool.set(3, forKey: "whichOne")
+            print("yah2")
+        }
+        //Distance Segmented Control
+        let choseDist = [1, 2, 3, 4]
+        let resultTwo = choseDist[distanceControl.selectedSegmentIndex]
+        if(resultTwo == 1){
+            savedBool.set(0, forKey: "whichTwo")
+            print("yah")
+        }
+        else if(resultTwo == 2){
+            savedBool.set(1, forKey: "whichTwo")
+            print("yah1")
+        }
+        else if(resultTwo == 3){
+            savedBool.set(2, forKey: "whichTwo")
+            print("yah1")
+        }
+        else{
+            savedBool.set(3, forKey: "whichTwo")
+            print("yah2")
+        }
+        self.dismiss(animated: true, completion: nil)
+        
         
     }
-    
-    @IBAction func costAction(_ sender: Any) {
-    }
-    
-    @IBAction func distanceAction(_ sender: Any) {
-    }
-    
-    
     
     
    
